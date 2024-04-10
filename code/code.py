9a8cffda4d7dfd5adcd4365bf821e1ba393e81bc
amu <fuerchti@gmail.com>
2024-04-10 19:47:44 +0100

    fixed issue with board (rx / tx swap for the sd card)

--- code/code.py
+++ code/code.py
@@ -12,22 +12,23 @@ import mfrc522
 import alarm
 import os
 
-# RFID card reader
-spi_rfid = busio.SPI(board.GP18, board.GP19, board.GP16) #clock, mosi, miso
-rfid = mfrc522.MFRC522(spi_rfid, board.GP28, board.GP17)
 
-while True:
-    (status, tag_type) = rfid.request(rfid.REQIDL)
-    if status == rfid.OK:
-        (stat, raw_uid) = rfid.anticoll()
-        while (stat == rfid.OK):
-            uid = "0x" + "%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
-            path = "/sd/" + "%02x%02x%02x%02x.wav" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
-            print("uuid: "+uid)
-            time.sleep(1)
-            (stat, raw_uid) = rfid.anticoll()
-        print("not ok anticoll")
-    else:
-        print("not ok requidl")
-    time.sleep(1)
+amp_power = digitalio.DigitalInOut(board.GP26)
+amp_power.switch_to_output()
+
+
+# I2S audio output
+audio = audiobusio.I2SOut(bit_clock=board.GP20, word_select=board.GP21, data=board.GP22)
+
+# Make sure the amp is powered
+amp_power.value = True
+
+with open("/unknown_card.wav", "rb") as wave_file:
+    wave = audiocore.WaveFile(wave_file)
+    audio.play(wave)
+    while audio.playing:
+        time.sleep(0.5)
+    audio.stop()
+
+amp_power.value = False
 
