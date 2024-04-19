56d84b850988b7e8bdb342e418443e3a2f711f66
amu <fuerchti@gmail.com>
2024-04-19 21:09:20 +0100

    Updated docs

--- README.md
+++ README.md
@@ -1,3 +1,46 @@
 # Music Box
 
-Template readme
+A rfid based player I made. It basically scans for RFID takes, and looks for a WAV file on the SD card with that ID, if found it plays it.
+The WAV files need to be 22khz, 16bit mono. Audacity is a good tool to convert mp3s.
+
+## Board
+
+The board is simply a pico with and SD card and the I2S chip.
+
+See KiCad files.
+
+### Schematic
+
+![Schematic](img/schematic.png)
+
+### PCB
+
+![Schematic](img/pcb.png)
+
+### BOM
+
+<table>
+<tr><td>CR1</td><td>PMEG2005AEA</td><td>https://uk.farnell.com/nexperia/pmeg2005aea-115/diode-schottky-sod-323/dp/8737843?st=pmeg2005aea</td></tr>
+<tr><td>J2</td><td>GCT MEM2061-01-188-00-A</td><td>https://uk.farnell.com/gct-global-connector-technology/mem2061-01-188-00-a/micro-sd-conn-push-push-8pos-smt/dp/2678544?ost=mem2061-01-188-00-a</td></tr>
+<tr><td>C6, C2</td><td>0.1uf, 0603</td><td></td></tr>
+<tr><td>C5, C1</td><td>10uf, 0805</td><td></td></tr>
+<tr><td>R2</td><td>Array, 4x 47k (EXB-N8V473JX)</td><td>https://uk.farnell.com/panasonic/exbn8v473jx/resistor-ccav-array-0402x4-47k/dp/2060146?st=EXBN8V473JX</td></tr>
+<tr><td>R6</td><td>649k, 0402</td><td></td></tr>
+<tr><td>U1</td><td>I2S Amp, MAX98357AETE+T</td><td>https://uk.farnell.com/analog-devices/max98357aete-t/audio-power-amp-d-40-to-85deg/dp/2949165?st=MAX98357AETE%20T</td></tr>
+<tr><td>FB1, FB2</td><td>Ferrite Beads, MMZ1608B121CTDH5</td><td>https://uk.farnell.com/tdk/mmz1608b121ctdh5/ferrite-chip-bead-1608-0-15-ohm/dp/3386884?st=mmz1608b121ctdh5</td></tr>
+<tr><td>C4,C3</td><td>220pf, 0402</td><td></td></tr>
+<tr><td>J1</td><td>JST, XH 2.5mm</td><td></td></tr>
+<tr><td>J4, J3, J5</td><td>JST, PH, 2mm</td><td></td></tr>
+
+</table>
+
+
+## Software
+
+Flash circuit python (originally I used v9) and copy the lib folder and `code.py` files over. That should be it.
+
+
+## Case
+
+See FreeCad files in `3dPrinting`
+
--- code/code.py
+++ code/code.py
@@ -12,23 +12,73 @@ import mfrc522
 import alarm
 import os
 
+print("Running music box")
 
+# SPI for the SD card
+spi_sd = busio.SPI(clock = board.GP10, MOSI = board.GP11, MISO = board.GP12) #clock, mosi, miso
+# Select pin for the SD card
+sd_select = digitalio.DigitalInOut(board.GP13)
+# SD card lib
+sdcard = adafruit_sdcard.SDCard(spi_sd, sd_select)
+
+while True:
+  try:
+    # mount the sd card to /sd
+    vfs = storage.VfsFat(sdcard)
+    storage.mount(vfs, "/sd")
+    break
+  except:
+    print("Error mounting SD card")
+    time.sleep(1)
+
+# RFID card reader
+spi_rfid = busio.SPI(board.GP18, board.GP19, board.GP16) #clock, mosi, miso
+rfid = mfrc522.MFRC522(spi_rfid, board.GP28, board.GP17)
+
+
+# I2S audio output & amp
 amp_power = digitalio.DigitalInOut(board.GP26)
 amp_power.switch_to_output()
-
-
-# I2S audio output
 audio = audiobusio.I2SOut(bit_clock=board.GP20, word_select=board.GP21, data=board.GP22)
 
 # Make sure the amp is powered
 amp_power.value = True
 
-with open("/unknown_card.wav", "rb") as wave_file:
-    wave = audiocore.WaveFile(wave_file)
-    audio.play(wave)
-    while audio.playing:
-        time.sleep(0.5)
-    audio.stop()
 
-amp_power.value = False
+while True:
+  (status, tag_type) = rfid.request(rfid.REQIDL)
+  if status == rfid.OK:
+    (stat, raw_uid) = rfid.anticoll()
+    if (stat == rfid.OK):
+      uid = "0x" + "%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
+      path = "/sd/" + "%02x%02x%02x%02x.wav" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
+      try:
+        os.stat(path)
+      except:
+        path = "/unknown_card.wav"
+
+      print("Playing path: "+path)
+
+      with open(path, "rb") as wave_file:
+        wave = audiocore.WaveFile(wave_file)
+        audio.play(wave)
+        while (audio.playing and stat == rfid.OK):
+          time.sleep(0.5)
+          (stat, raw_uid) = rfid.anticoll()
+
+        print("Stopping audio path: "+path)
+        audio.stop()
+  #
+  #   while (stat == rfid.OK):
+  #     uid = "0x" + "%02x%02x%02x%02x" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
+  #     path = "/sd/" + "%02x%02x%02x%02x.wav" % (raw_uid[0], raw_uid[1], raw_uid[2], raw_uid[3])
+  #     print("uuid: "+uid)
+  #     time.sleep(1)
+  #     (stat, raw_uid) = rfid.anticoll()
+  #   print("not ok anticoll")
+  # else:
+  #   print("not ok requidl")
+  time.sleep(1)
+
+
 
